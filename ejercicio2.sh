715991a52c7ab7a17c47dccbcbbf7d3ab9e01b32
Roger Ivan Gurdian <gurdian@localhost.localdomain>
2021-02-10 19:40:46 -0600

    Avances

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
+clear
+
 echo "**********************************"
-echo "*     CREACION DE DIRECTORIO     *"
+echo "*             MENU               *"
 echo "* 1. Crear carpeta               *"
 echo "* 2. Eliminar carpeta            *"
 echo "**********************************"
 echo "Escoja la opcion:"
 read opcion
 
-if $opcion == 1; then
+clear
+
+if [ "$opcion" = 1 ]; then
+    echo "**********************************"
+    echo "*         CREAR CARPETA          *"
+    echo "**********************************"
     echo "Ingrese nombre de la carpeta:"
     read carpeta
-    mkdir $HOME/$carpeta
-    echo "Carpeta creada correctamente, la ruta es:" $HOME/$carpeta
+    echo "Ingrese la ruta de la carpeta:"
+    read ruta
+
+    rutaCarpeta=$ruta$carpeta
+
+    mkdir $rutaCarpeta
+    echo "Carpeta creada correctamente, la ruta es:" $rutaCarpeta
+
+    # if [ -d $rutCarpeta ]; then
+    #     echo "No se puede crear el directorio, $rutaCarpeta ya existe"
+    # else
+    #     mkdir $rutaCarpeta
+    #     echo "Carpeta creada correctamente, la ruta es:" $rutaCarpeta
+    # fi
 fi
