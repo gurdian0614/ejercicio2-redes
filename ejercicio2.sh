f50f2f3a0988448bfc47c19a6c8220b6bc892df2
gurdian0614 <gurdian@localhost.localdomain>
2021-02-10 20:14:56 -0600

    Finalizando ejercicio

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -5,31 +5,52 @@ echo "**********************************"
 echo "*             MENU               *"
 echo "* 1. Crear carpeta               *"
 echo "* 2. Eliminar carpeta            *"
+echo "* 3. Salir                       *"
 echo "**********************************"
 echo "Escoja la opcion:"
 read opcion
 
-clear
+while [ "$opcion" != 3 ]
+do
+    clear
 
-if [ "$opcion" = 1 ]; then
+    if [ "$opcion" = 1 ]; then
+        echo "**********************************"
+        echo "*         CREAR CARPETA          *"
+        echo "**********************************"
+        echo "Ingrese la ruta de la carpeta:"
+        read ruta
+
+        rutaCarpeta=$ruta
+
+        mkdir $rutaCarpeta
+        echo "Carpeta creada correctamente, la ruta es:" $rutaCarpeta
+
+        # if [ -d $rutCarpeta ]; then
+        #     echo "No se puede crear el directorio, $rutaCarpeta ya existe"
+        # else
+        #     mkdir $rutaCarpeta
+        #     echo "Carpeta creada correctamente, la ruta es:" $rutaCarpeta
+        # fi
+    elif [ "$opcion" == 2 ]; then
+        echo "**********************************"
+        echo "*       ELIMINAR CARPETA         *"
+        echo "**********************************"
+        echo "Ingrese la ruta de la carpeta:"
+        read ruta
+
+        rmdir $ruta
+        echo "La carpeta ha sido eliminada correctamente"
+    fi
+
     echo "**********************************"
-    echo "*         CREAR CARPETA          *"
+    echo "*             MENU               *"
+    echo "* 1. Crear carpeta               *"
+    echo "* 2. Eliminar carpeta            *"
+    echo "* 3. Salir                       *"
     echo "**********************************"
-    echo "Ingrese nombre de la carpeta:"
-    read carpeta
-    echo "Ingrese la ruta de la carpeta:"
-    read ruta
+    echo "Escoja la opcion:"
+    read opcion
+done
 
-    rutaCarpeta=$ruta$carpeta
-
-    mkdir $rutaCarpeta
-    echo "Carpeta creada correctamente, la ruta es:" $rutaCarpeta
-
-    # if [ -d $rutCarpeta ]; then
-    #     echo "No se puede crear el directorio, $rutaCarpeta ya existe"
-    # else
-    #     mkdir $rutaCarpeta
-    #     echo "Carpeta creada correctamente, la ruta es:" $rutaCarpeta
-    # fi
-    #
-fi
+echo "ADIOS!!!"
